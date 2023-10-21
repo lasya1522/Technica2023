0c64a11717aae6d6d5b9fa7cef3e56b7266cfa7a
Anvi Padiyar <anvipadiyar@gmail.com>
2023-10-21 15:26:15 -0400

    index pls work

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
-import web_scraping
+#import web_scraping
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+if __name__ == "__main__":
+ app.run(debug=True)
