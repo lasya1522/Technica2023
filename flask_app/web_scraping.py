3a9c54734c557da93f510b1a8f2b7b55d60a9e94
Lasya Muthyam <lasya1522@gmail.com>
2023-10-21 13:52:05 -0400

    Retrives all text from website as html and get can title

--- flask_app/web_scraping.py
+++ flask_app/web_scraping.py
@@ -1,1 +1,48 @@
 #web scraping file
+from bs4 import BeautifulSoup
+import requests
+
+from urllib.request import urlopen
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+html = urlopen('https://www.umpd.umd.edu/stats/csa_logs.cfm')
+print(html.read())
+
+# parse text
+soup = BeautifulSoup(html.read(), 'html.parser')
+print(soup.prettyfy())
+
+try:
+    html = urlopen("https://pythonscrapingthisurldoesnotexist.com")
+except HTTPError as e:
+    print("The server returned an HTTP error")
+except URLError as e:
+    print("The server could not be found!")
+else:
+    print(html.read())
+
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(), "lxml")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+
+title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
