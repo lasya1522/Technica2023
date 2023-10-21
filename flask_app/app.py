579fe7996e92f8eaff8608314ddcf87a48503c18
sriyayay <sriya.peddinti@gmail.com>
2023-10-21 17:21:17 -0400

    app.py save

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
+#import web_scraping
 
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/stats')
+def stats():
+    return render_template("stats.html")
+
+
+@app.route('/map')
+def stats():
+    return render_template("map.html")
 
 if __name__ == "__main__":
  app.run(debug=True)
